70640e82a9fb96a782cf817dfd60259478ce0dbf
YCH-3ye <281242411@qq.com>
2020-02-06 07:51:47 +0800

    [M]home第二种导航完成

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { NavLink, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 // 引入子组件
 import House from './House'
@@ -7,45 +7,66 @@ import Index from './Index/index'
 import News from './News'
 import My from './My'
 
+// 引入react组件
+import { TabBar } from 'antd-mobile';
+
 import './index.scss'
+
+const navBarList = [
+  { title: '首页', icon: 'icon-ind', path: '/home' },
+  { title: '找房', icon: 'icon-findHouse', path: '/home/house' },
+  { title: '资讯', icon: 'icon-infom', path: '/home/news' },
+  { title: '我的', icon: 'icon-my', path: '/home/my' }
+]
 
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: props.location.pathname
+    }
+  }
+  renderNavBar() {
+    return navBarList.map(item => (
+        <TabBar.Item
+          title={item.title}
+          key={item.title}
+          icon={ <i className={`iconfont ${item.icon}`}></i>
+          }
+          selectedIcon={<i className={`iconfont ${item.icon}`}></i>
+          }
+          selected={this.state.selectedTab === item.path}
+          onPress={() => {
+            this.props.history.push(item.path)
+            this.setState({
+              selectedTab: item.path
+            });
+          }}
+        >
+        </TabBar.Item>
+      ))
+  }
   render() {
     return (
       <>
-        <ul className="navClass">
-          <li>
-            <NavLink exact to="/home">
-              <i className="iconfont icon-ind" />
-              <p>首页</p>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/home/house">
-              <i className="iconfont icon-findHouse" />
-              <p>找房</p>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/home/news">
-              <i className="iconfont icon-infom" />
-              <p>资讯</p>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/home/my">
-              <i className="iconfont icon-my" />
-              <p>我的</p>
-            </NavLink>
-          </li>
-        </ul>
         <Switch>
           <Route exact path="/home" component={Index} />
           <Route path="/home/house" component={House} />
           <Route path="/home/news" component={News} />
           <Route path="/home/my" component={My} />
         </Switch>
+        <div className="NavBar">
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
+            hidden={this.state.hidden}
+          >
+            {this.renderNavBar()}
+          </TabBar>
+        </div>
+
       </>
     )
   }
